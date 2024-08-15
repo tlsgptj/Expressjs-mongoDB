f35f7279a69aef957773b9336356c50b14bd49ca
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-15 19:50:33 +0900

    몽고디비 추가

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-## Express 뼈대 코드 정리
+### Express 뼈대 코드 정리
 import express from 'express';
 const app = express();
-//라우트 정의
+
+### 라우트 정의
 app.listen (3000, () => console.log('Server Started'));
 app.method(path, handler)
 1. method : HTTP 메소드 이름
 2. path : 엔드포인트 경로
 3. handler : 리퀘스트 로직을 처리, 리스폰스 돌려주는 핸들러 함수
-## 리퀘스트 객체 
+
+### 리퀘스트 객체 
 req.query 
 쿼리스트링 파라미터를 프로퍼티로 담고있는 객체 
 req.params
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import tasks from './data/mock.js';
+import mongoose from 'mongoose';
+import { DATABASE_URL } from './env.js';
 
 const app = express();
 app.use(express.json());
@@ -64,4 +66,5 @@ app.patch('/tasks/:id', (req, res) => {
     
 });
 
+mongoose.connect(DATABASE_URL).then(() => console.log('Connected to DB'));
 app.listen(3000, () => console.log('Server Started'));
