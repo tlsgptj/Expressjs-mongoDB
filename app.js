ebece87a254d1fb26e7062706e8fc72cc2917f84
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-17 13:28:59 +0900

    몽구스 쿼리 공부

--- app.js
+++ app.js
@@ -2,34 +2,28 @@ import express from 'express';
 import tasks from './data/mock.js';
 import mongoose from 'mongoose';
 import { DATABASE_URL } from './env.js';
+import Task from './models/tasks.js';
 
 const app = express();
 app.use(express.json());
-app.get('/tasks', (req, res) => {
+app.get('/tasks', async (req, res) => {
     /**
      * 쿼리 파라미터 
      * - sort : 'oldest인 경우 오래된 태스크 기준, 나머지 경우 새로운 태스크 기준
      * - count : 태스크 개수
      */
-    const sort = Number(req.query.sort);
-    const count = Number(req.query.count);
-    const compareFn = 
-        sort == 'oldest'
-        ? (a, b) => a.createdAt - b.createdAt
-        : (a, b) => b.createdAt - a.createdAt
+    const sort = req.query.sort;
+    const count = Number(req.query.count) || 0;
+    const sortOption = { createdAt: sort === 'oldest' ? 'asc' : 'desc' }; //객체 생성
+    const tasks = await Task.find().sort(sortOption).limit(count);
+    //쿼리를 리턴하기 때문에 메소드를 연결해서 사용 가능함 -> 최종결과 쿼리
 
-    let newTasks = tasks.sort(compareFn);
-    
-    if (count) {
-        newTasks = newTasks.slice(0, count);
-    }
-
-    res.send(newTasks);
+    res.send(tasks);
 });
 
-app.get('/tasks/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const task = tasks.find((task) => task.id === id);
+app.get('/tasks/:id', async (req, res) => {
+    const id = req.params.id;
+    const task = await Task.findById(id); //쿼리를 리턴, await을 이용해 결과를 리턴 
     if (task) {
         res.send(task);
     } else {
